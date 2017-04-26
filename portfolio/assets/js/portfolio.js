83fd206f234e87f6d44a1bf93c6832c7a47aca22
juliezwang <default_user@airbears2-10-142-141-68.airbears2.1918.berkeley.edu>
2017-04-25 23:32:11 -0700

    Revert "My First Website"
    This reverts commit be155e7934712e054035b2252cd25328f5c9f5c0.

--- portfolio/assets/js/portfolio.js
+++ portfolio/assets/js/portfolio.js
@@ -2,12 +2,12 @@ $(document).ready(function() {
 
 $('.question').click(function() {
 		if ($(this).parent().is('.open')){
-			$(this).closest('.question1').find('.answer_container').animate({'height':'0'},800);
-			$(this).closest('.question1').removeClass('open');
+			$(this).closest('.faqs').find('.answer_container').animate({'height':'0'},800);
+			$(this).closest('.faqs').removeClass('open');
 			} else{
-				var newHeight =$(this).closest('.question1').find('.answer').height() +'px';
-				$(this).closest('.question1').find('.answer_container').animate({'height':newHeight},800);
-				$(this).closest('.question1').addClass('open');
+				var newHeight =$(this).closest('.faqs').find('.answer').height() +'px';
+				$(this).closest('.faqs').find('.answer_container').animate({'height':newHeight},800);
+				$(this).closest('.faqs').addClass('open');
 			}
 })
 });
